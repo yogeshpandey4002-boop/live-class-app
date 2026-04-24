c8285fe144b8a9a54899e01cc32f516bc533d96d
yogeshpandey4002-boop <yogeshpandey4002@gmail.com>
2026-04-24 07:37:32 +0530

    Update server to serve HTML pages and remove socket.io
    Refactor server to serve static files and handle routes.

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const path = require("path");
 
 app.use(express.static(__dirname));
 
-io.on("connection", socket => {
-
-  socket.on("join-room", room => {
-    socket.join(room);
-
-    socket.to(room).emit("user-joined", socket.id);
-
-    socket.on("signal", data => {
-      socket.to(room).emit("signal", data);
-    });
-
-    socket.on("chat", msg => {
-      io.to(room).emit("chat", msg);
-    });
-
-    socket.on("disconnect", () => {
-      socket.to(room).emit("user-left", socket.id);
-    });
-
-  });
-
+// Home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(3000, () => console.log("Server running"));
+// Room page
+app.get("/room", (req, res) => {
+  res.sendFile(path.join(__dirname, "room.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
